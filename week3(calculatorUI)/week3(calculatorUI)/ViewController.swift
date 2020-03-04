a2baa1a037be3835b5ab4d4ce164aeffd34554ce
xiyezry <xiyezry@qq.com>
2020-03-04 15:02:04 +0800

    initialise the project.

---
+++ week3(calculatorUI)/week3(calculatorUI)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week3(calculatorUI)
+//
+//  Created by 钟仁毅 on 2020/3/4.
+//  Copyright © 2020 钟仁毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
